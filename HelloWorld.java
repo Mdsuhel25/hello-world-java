88ae3a42235e955e251996942513b722f4b25dfa
Mdsuhel25 <82922556+Mdsuhel25@users.noreply.github.com>
2022-12-07 13:29:48 +0530

    new commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
      System.out.println("New changes have been made to see in jenkins");
+     System.out.println("Poll-scm will be trigger at specific time when the code is modified");
   }
 }
