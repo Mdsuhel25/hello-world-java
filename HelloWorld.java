235ae6cecbe9533d40eb4f11cb3281a9a8fa57a2
Mdsuhel25 <82922556+Mdsuhel25@users.noreply.github.com>
2022-12-07 14:55:14 +0530

    new changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     System.out.println("Hello world!");
      System.out.println("New changes have been made to see in jenkins");
      System.out.println("Poll-scm will be trigger at specific time when the code is modified");
+     System.out.println("Poll-scm will be trigger at specific time when the code is modified");
   }
 }
